6a5c0e5095f5d03381485e209f87d6b45d729ad0
El Gallo Gallo <loscursosdearquimedes@gmail.com>
2020-09-05 10:40:44 +0000

    Bot v1.5

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+5const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Yeah'));
+app.get('/', (req, res) => res.send('Hey'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
